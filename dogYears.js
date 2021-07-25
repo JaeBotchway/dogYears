d3b293ca65bf3543f0587277af5e49292e69d0d4
JaeBotchway <jacqueline.botchway@amalitech.org>
2021-07-25 19:44:59 +0000

    first commit

---
+++ dogYears.js
@@ -0,0 +1,18 @@
+//variable holding age value
+let myAge = 19
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+//subtract 2 from myage variable
+let laterYears = myAge - 2
+//number of dog years accounted for
+laterYears *= 4
+console.log(`${earlyYears} ${laterYears}`)
+
+//add earlyyears and lateryears
+let myAgeInDogYears = earlyYears + laterYears
+
+//toLowerCase() returns a string with lowercase
+let myName = 'Jae'.toLowerCase()
+
+//console log output
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years in dog years`)
